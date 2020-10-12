fa847529e4d6d06d2dc8bf97eb4c5b8d9c39b052
atamaniac <38379857+atamaniac@users.noreply.github.com>
2020-10-12 13:44:27 +0200

    playing with the basic tests

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -13,12 +14,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         # return last 5 published questions
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
